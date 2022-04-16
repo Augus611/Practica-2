fb5029dd65b6223629a1391e6e37e453899c9738
Augus611 <augus611@gmail.com>
2022-04-16 20:49:46 -0300

    Creé la función numeroAleatorio.
    Genera un número aleatorio entre 1 y 5 y lo compara con el ingresado por el usuario. Luego, si son distintos resta un intento, y si son iguales los reseta.

--- app/src/main/java/com/grupo4/practica2/Ejercicio1.kt
+++ app/src/main/java/com/grupo4/practica2/Ejercicio1.kt
@@ -2,10 +2,32 @@ package com.grupo4.practica2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
 
 class Ejercicio1 : AppCompatActivity() {
+    private var intentos = 5
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ejercicio1)
+    }
+    fun numeroAleatorio(view: View) {
+        // Genero un número aleatorio entre 1 y 5
+        val numeroAleatorio = (1..5).random()
+        // Obtengo el número ingresado por el usuario. Si no ingresó número, lo igualo a 0.
+        val numeroUsuario = findViewById<EditText>(R.id.input_numero).text.toString().toIntOrNull() ?: 0
+        // Comparo ambos números
+        if (numeroAleatorio == numeroUsuario) {
+            // sumar 10 puntos
+            intentos = 5
+            return
+        } else {
+            intentos--
+            if (intentos == 0) {
+                // terminar el juego
+                intentos = 5
+                return
+            }
+        }
     }
 }
