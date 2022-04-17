36fc19b0a1d69ed565b9d05e6dd3531ea50ac979
Augus611 <augus611@gmail.com>
2022-04-17 01:16:49 -0300

    La funcion numeroAleatorio ahora guarda el puntaje máximo.

--- app/src/main/java/com/grupo4/practica2/Ejercicio1.kt
+++ app/src/main/java/com/grupo4/practica2/Ejercicio1.kt
@@ -1,30 +1,40 @@
 package com.grupo4.practica2
 
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
+import android.widget.Toast
 
 class Ejercicio1 : AppCompatActivity() {
     private var intentos = 5
+    private var puntajeActual = 0
+    private lateinit var sharedPreferences : SharedPreferences
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ejercicio1)
+        sharedPreferences = getSharedPreferences("puntajeMaximo", Context.MODE_PRIVATE)
     }
     fun numeroAleatorio(view: View) {
-        // Genero un número aleatorio entre 1 y 5
+        // Genera un número aleatorio del 1 al 5 y lo compara con el ingresado por el usuario.
         val numeroAleatorio = (1..5).random()
-        // Obtengo el número ingresado por el usuario. Si no ingresó número, lo igualo a 0.
         val numeroUsuario = findViewById<EditText>(R.id.input_numero).text.toString().toIntOrNull() ?: 0
-        // Comparo ambos números
         if (numeroAleatorio == numeroUsuario) {
-            // sumar 10 puntos
+            puntajeActual += 10
             intentos = 5
             return
         } else {
             intentos--
             if (intentos == 0) {
-                // terminar el juego
+                val puntajeMaximo = sharedPreferences.getInt("puntajeMaximo", 0)
+                if (puntajeActual > puntajeMaximo) {
+                    val edit = sharedPreferences.edit()
+                    edit.putInt("puntajeMaximo", puntajeActual)
+                    edit.apply()
+                    Toast.makeText(this,"¡Nuevo puntaje máximo!",Toast.LENGTH_SHORT).show()
+                }
                 intentos = 5
                 return
             }
