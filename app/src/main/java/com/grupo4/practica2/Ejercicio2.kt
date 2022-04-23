2a75331f0bb467ce4b65c5ec7d49511415728524
juanpaladino15 <juanpaladino15@gmail.com>
2022-04-23 18:55:08 -0300

    Avance sobre ejercicio 2 (Solo la vista)

---
+++ app/src/main/java/com/grupo4/practica2/Ejercicio2.kt
@@ -0,0 +1,11 @@
+package com.grupo4.practica2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Ejercicio2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ejercicio2)
+    }
+}
